26d1a3204c58259a5d3469ee84e6a8b7f5fd15d0
Antti Veikkolainen <aveboy.00@gmail.com>
2022-06-09 22:55:27 +0300

    Add project code

---
+++ OpenInLinqPad/Command.cs
@@ -0,0 +1,138 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using Task = System.Threading.Tasks.Task;
+
+using System.IO;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using Microsoft;
+using System.Text;
+
+namespace OpenInLinqPad
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class Command
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("f63dbbb5-e0da-497f-b30b-37e448064b42");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Command"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private Command(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static Command Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in OpenInLinqPad's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new Command(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            var dte = (DTE2)ServiceProvider.GetServiceAsync(typeof(DTE)).Result;
+
+            Assumes.Present(dte);
+            if (dte.ActiveDocument?.Selection is TextSelection selection)
+            {
+                
+                MessageBox.Show(selection.Text);
+
+                string pathToTemp = System.IO.Path.GetTempPath();
+                string id = Guid.NewGuid().ToString();
+
+                string tempFilePath = System.IO.Path.Combine(pathToTemp, $"{id}.cs");
+
+                File.WriteAllText(tempFilePath, selection.Text);
+
+                var app = @"LINQPad7.exe";
+
+                string command = $"/c start {app} {tempFilePath}";
+
+                var processInfo = new ProcessStartInfo("cmd.exe", command);
+                processInfo.CreateNoWindow = false;
+                processInfo.UseShellExecute = false;
+
+                var process = System.Diagnostics.Process.Start(processInfo);
+
+                process.WaitForExit();
+
+                Console.WriteLine("ExitCode: {0}", process.ExitCode);
+                process.Close();
+            }
+            else
+            {
+                MessageBox.Show("Selection was empty");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # OpenInLinqPad
+
+To add the command to the editor context menu:
+Go to Tools -> Customize -> Commands
+
+Click the "Context menu:" radio button
+Select "Editor Context Menus | Code Window"
+Click "Add Command..."
+Go to Tools
+Find OpenInLinqPad
+
+Now close the customize window and you're done.
+Select some code and right click the editor code window
+Press OpenInLinqPad to see the extension in action
